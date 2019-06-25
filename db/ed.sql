948ba0a47a40af17ce0f5b59cd185bfc9d223692
waqas saleem <waqas.saleem7431@gmail.com>
2019-06-25 16:55:03 +0500

    Update ed.sql

--- db/ed.sql
+++ db/ed.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 31, 2019 at 08:21 PM
+-- Generation Time: Jun 25, 2019 at 01:53 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -25,6 +25,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(100) NOT NULL,
+  `category_name` varchar(256) DEFAULT NULL,
+  `category_slug` varchar(256) DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category_name`, `category_slug`, `date`) VALUES
+(4, 'website design', 'website-design', '2019-06-11 00:04:02'),
+(5, 'Logo Design', 'logo-design', '2019-06-11 00:04:11'),
+(6, 'Content Writter', 'content-writter', '2019-06-11 00:04:22'),
+(7, 'Data Entery', 'data-entery', '2019-06-11 00:04:44'),
+(8, 'iOS App', 'ios-app', '2019-06-11 00:37:40'),
+(9, 'Android App', 'android-app', '2019-06-11 00:37:50'),
+(10, 'Web Researcher', 'web-researcher', '2019-06-11 00:37:56'),
+(11, 'Wordpress', 'wordpress', '2019-06-11 00:38:39');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `gig_data`
 --
 
@@ -33,12 +60,14 @@ CREATE TABLE `gig_data` (
   `username` varchar(256) NOT NULL,
   `niche_title` varchar(256) NOT NULL,
   `niche_category` varchar(256) NOT NULL,
+  `niche_cat_slug` varchar(256) DEFAULT NULL,
   `niche_price` int(11) NOT NULL,
   `niche_tag` varchar(256) NOT NULL,
   `niche_image` varchar(256) NOT NULL,
   `niche_desc` varchar(256) NOT NULL,
   `niche_rating` varchar(256) NOT NULL DEFAULT '0',
   `order_completed` varchar(256) NOT NULL DEFAULT '0',
+  `order_in_queue` varchar(256) NOT NULL DEFAULT '0',
   `niche_add_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -46,13 +75,14 @@ CREATE TABLE `gig_data` (
 -- Dumping data for table `gig_data`
 --
 
-INSERT INTO `gig_data` (`id`, `username`, `niche_title`, `niche_category`, `niche_price`, `niche_tag`, `niche_image`, `niche_desc`, `niche_rating`, `order_completed`, `niche_add_date`) VALUES
-(4, '', 'niche title', 'Logo Design', 76, 'wordpress, web design, website', '6-important-factors-in-building-ecommerce-websites.jpg', '<p>vsdckjs</p>', '0', '0', '2019-05-22 21:23:31'),
-(5, '', 'WordPress Developer', 'WordPress', 46, 'wordpress, web design, website', 'Template-theme-image-electro.jpg', '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolo', '0', '0', '2019-05-22 21:46:09'),
-(6, '', 'Logo Designer', 'Logo Design', 87, 'logo, designer, photoshop', 'bostan-minimal-business-wordpress-website-theme.jpg', '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolo', '0', '0', '2019-05-22 21:46:52'),
-(7, 'admin', 'Content Writer ', 'Content Writter', 23, 'content, writer', 'business-women-crisis.jpg', '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolo', '0', '0', '2019-05-22 21:47:26'),
-(8, 'admin', 'niche title', 'WordPress', 5, 'wordpress, web design, website', 'bostan-minimal-business-wordpress-website-theme.jpg', '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolo', '0', '0', '2019-05-22 21:53:41'),
-(9, 'admin', 'I will design a professional wordpress website', 'WordPress', 75, 'wordpress, web design, website', '6-important-factors-in-building-ecommerce-websites.jpg', '<p><strong>Lorem ipsum dolor sit amet, consectetur ad</strong>ipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Dui', '0', '0', '2019-05-25 01:08:59');
+INSERT INTO `gig_data` (`id`, `username`, `niche_title`, `niche_category`, `niche_cat_slug`, `niche_price`, `niche_tag`, `niche_image`, `niche_desc`, `niche_rating`, `order_completed`, `order_in_queue`, `niche_add_date`) VALUES
+(0, 'admin', 'I will do data entry work', 'Data Entery', 'data-entery', 5, 'data, entry, excel', '6-important-factors-in-building-ecommerce-websites.jpg', '<p><strong>i will do data entery work</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e', '0', '0', '0', '2019-06-11 00:59:17'),
+(4, '', 'niche title', 'Logo Design', 'logo-design', 76, 'wordpress, web design, website', '6-important-factors-in-building-ecommerce-websites.jpg', '<p>vsdckjs</p>', '0', '0', '0', '2019-05-22 21:23:31'),
+(5, '', 'WordPress Developer', 'WordPress', 'wordpress', 46, 'wordpress, web design, website', 'Template-theme-image-electro.jpg', '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolo', '0', '0', '0', '2019-05-22 21:46:09'),
+(6, '', 'Logo Designer', 'Logo Design', 'logo-design', 87, 'logo, designer, photoshop', 'bostan-minimal-business-wordpress-website-theme.jpg', '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolo', '0', '0', '0', '2019-05-22 21:46:52'),
+(7, 'admin', 'Content Writer ', 'Content Writter', 'content-writter', 23, 'content, writer', 'business-women-crisis.jpg', '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolo', '0', '0', '0', '2019-05-22 21:47:26'),
+(8, 'admin', 'niche title', 'WordPress', 'wordpress', 5, 'wordpress, web design, website', 'bostan-minimal-business-wordpress-website-theme.jpg', '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolo', '0', '0', '0', '2019-05-22 21:53:41'),
+(9, 'admin', 'I will design a professional wordpress website', 'WordPress', 'wordpress', 75, 'wordpress, web design, website', '6-important-factors-in-building-ecommerce-websites.jpg', '<p><strong>Lorem ipsum dolor sit amet, consectetur ad</strong>ipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Dui', '0', '0', '0', '2019-05-25 01:08:59');
 
 -- --------------------------------------------------------
 
@@ -93,8 +123,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `fname`, `lname`, `username`, `email`, `password`, `phone`, `profile_title`, `skills`, `certificatename`, `certificateimage`, `securityquestion`, `security_answer`, `profileimage`, `total_order_completed`, `profile_info`, `profile_status`, `profile_level`, `success_score`, `deliver_on_time`, `order_completetion`, `rating`, `user_ip`, `date`) VALUES
 (1, 'Aleem', 'Sharif', 'Aleemsharif', 'iamaleemsharif@gmail.com', 'Examination2015', '', NULL, '', '', '', '', NULL, '', 0, '', 'pending', 0, 0, 0, 0, 0, NULL, '2019-04-24 20:07:54'),
-(4, 'waqas', 'saleem', 'admin', 'waqas.saleem7431@gmail.com', '1234', '+923037156697', 'Professional WordPress Developer', '', 'Udemy web development', 'certificate_admin_1558899449.jpg', 'Your Pet Name?', 'Puppy', 'profile_admin_1558899449.jpg', 0, 'ok I am professional developer Test Waqas Saleem Write a brief description about you, your skills, your work experience. hjsdbfhsbdkjf Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo cons', 'verified', 0, 0, 0, 0, 0, NULL, '2019-05-25 10:24:44'),
-(6, 'waqas', 'saleem', 'admin2', 'black.hat7431@gmail.com', '123456789', '', 'Web developer test', '', '', '', '', NULL, '', 0, 'Write a brief description about you, your skills, your work experience.khjbnj', 'pending', 0, 0, 0, 0, 0, '::1', '2019-05-28 08:19:09');
+(4, 'waqas', 'saleem', 'admin', 'waqas.saleem7431@gmail.com', '1234', '+923037156697', 'Professional WordPress Developer', '', 'Udemy web development', 'certificate_admin_1558899449.jpg', 'Your Pet Name?', 'Puppy', 'profile_admin_1558899449.jpg', 0, 'ok I am professional developer Test Waqas Saleem Write a brief description about you, your skills, your work experience. hjsdbfhsbdkjf Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo cons', 'verified', 0, 0, 0, 0, 0, NULL, '2019-05-25 10:24:44');
 
 -- --------------------------------------------------------
 
@@ -127,6 +156,12 @@ INSERT INTO `verifications` (`id`, `username`, `email_verification_status`, `ema
 --
 
 --
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `gig_data`
 --
 ALTER TABLE `gig_data`
@@ -149,10 +184,16 @@ ALTER TABLE `verifications`
 --
 
 --
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `verifications`
